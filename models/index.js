97814bc2a808564c6c17372c4de7cbd6ca55e465
bdiallo10 <bdiallo10@icloud.com>
2020-09-20 22:57:51 -0700

    attempting to deploy app

--- models/index.js
+++ models/index.js
@@ -2,14 +2,13 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 // mongoose connection string
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { 
-    useNewUrlParser: true, 
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-    }
-);
+mongoose.connect(process.env.MONGODB_URI || 'http://localhost:3000');
+// mongoose.connect(uri, { 
+//     useNewUrlParser: true, 
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false
+//     });
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
